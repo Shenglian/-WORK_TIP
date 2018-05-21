043539b801c12d10bbcbb1254a73cae3d21316ee
Sheng <sheng.lin@appimc.com>
2018-05-21 12:55:31 +0800

    update

--- promise_example/index.js
+++ promise_example/index.js
@@ -1,8 +1,16 @@
-class {
+class Example {
   constructor() {
-
+    this.storyDiv = document.querySelector('.story');
   }
 
+  addHtmlToPage(content) {
+    let div = document.createElement('div');
+    div.innerHTML = content;
+    this.storyDiv.appendChild(div);
+  }
+
+  // https://googlesamples.github.io/web-fundamentals/fundamentals/primers/story.json
+  // https://shenglian.github.io/-WORK_TIP/promise_example/story.json
   get(url) {
     var requestPromise = new Promise(function(resolve, reject) {
       var req = new XMLHttpRequest();
@@ -19,12 +27,22 @@ class {
       req.send();
     });
 
-    return Promise.all([requestPromise]).then(results => {
-      console.log({results});
+    return Promise.all([requestPromise, requestPromise]).then(results => {
+      return results[1];
     })
   }
 
   getJSON(url) {
-    // return Promise.
+    console.log('this', this);
+    return this.get(url).then(JSON.parse)
   }
 }
+
+const _Example = new Example();
+
+_Example.getJSON('https://shenglian.github.io/-WORK_TIP/promise_example/story.json')
+  .then(res => {
+    _Example.addHtmlToPage(res.heading);
+    // console.log('story.chapterUrls.map(getJSON)', );
+    return res.chapterUrls.map(_Example.getJSON.bind(_Example))
+  })
